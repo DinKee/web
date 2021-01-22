08ea61de217478f5663a651849b2c08c2d93bb93
DinKee <ken123ken123k@gmail.com>
2021-01-22 23:46:48 +0800

    Final

--- project/index.js
+++ project/index.js
@@ -2,3 +2,18 @@
 var date = new Date().toLocaleString();
 $('#time').attr("data-date", date);
 $('#time').TimeCircles();
+
+//scrolling
+
+
+$('#about_me_link').click(function() {
+
+    $('html,body').animate({
+
+        scrollTop:$('#about_me').offset().top
+
+    }, 1000);
+
+    return false;
+
+});
